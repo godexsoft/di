4bb0b684f707c9128a4b295997b8c206223a2e62
Alex Kremer <godexsoft@gmail.com>
2022-12-02 04:41:52 +0000

    Add playgrounds for tests and benchmarks

---
+++ benchmarks/playground.cpp
@@ -0,0 +1,9 @@
+#include <di.hpp>
+
+#include <benchmark/benchmark.h>
+
+// static void Benchmark_MyBench(benchmark::State &state) {
+//     for(auto _ : state)
+//         benchmark::DoNotOptimize(di::Services<A, B, C>());
+// }
+// BENCHMARK(Benchmark_MyBench);
---
+++ tests/playground.cpp
@@ -0,0 +1,12 @@
+#include "types.hpp"
+#include <di.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace di;
+
+// TEST(Playground, MyTest) {
+// }
+
+TEST(Playground, MyTest) {
+}
